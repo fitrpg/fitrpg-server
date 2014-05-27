1d5a3523e2d8f9f5ae4da99e6ffe4739fcca91a1
Amira Anuar <amiraanuar@gmail.com>
2014-05-27 11:07:01 -0700

    Fix jawbone function to retrieve access token by replacing old variable names

--- server/fitbit/fitbit_controllers.js
+++ server/fitbit/fitbit_controllers.js
@@ -7,6 +7,14 @@ var FITBIT_CONSUMER_KEY = '8cda22173ee44a5bba066322ccd5ed34';
 var FITBIT_CONSUMER_SECRET = '12beae92a6da44bab17335de09843bc4';
 
 module.exports = exports = {
+  fitbitStrategy: new FitbitStrategy({
+    consumerKey: FITBIT_CONSUMER_KEY,
+    consumerSecret: FITBIT_CONSUMER_SECRET,
+    callbackURL: "/fitbit/authcallback"
+  },
+  function (token, tokenSecret, profile, done) {
+    done(null);
+  }),
   getTempToken: function (req, res, next) {
     console.log('Nothing happens here because it redirects to the Fitbit site.');
   },
@@ -15,13 +23,5 @@ module.exports = exports = {
     //then redirect with the token AND the user id and then make sure the client checks for
     // that and saves both things
     res.send(200,JSON.stringify({'Oauth Token Function':[]})); //this should never be viewed by the user, just ending the res
-  },
-  fitbitStrategy: new FitbitStrategy({
-    consumerKey: FITBIT_CONSUMER_KEY,
-    consumerSecret: FITBIT_CONSUMER_SECRET,
-    callbackURL: "/fitbit/authcallback"
-  },
-    function (token, tokenSecret, profile, done) {
-      done(null);
-    })
+  }
 };
