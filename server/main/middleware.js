43ed70116067205944de414a48992a45e6072c5d
Conor Fennell <fennell.conor@gmail.com>
2014-05-29 11:17:33 -0700

    Corrected CORS headers and POST and PUT for User now working

--- server/main/middleware.js
+++ server/main/middleware.js
@@ -22,7 +22,7 @@ module.exports = exports = {
   cors: function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Header', 'Content-type, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     if (req.method === 'Options') {
       res.send(200);
