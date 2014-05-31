1add433b2a57913e423759421ffb496c43bda8f2
Amira Anuar <amiraanuar@gmail.com>
2014-05-31 01:38:36 -0700

    add else to check username

--- server/user/user_controllers.js
+++ server/user/user_controllers.js
@@ -54,9 +54,15 @@ module.exports = exports = {
         console.log('user');
         console.log(user);
         if(user) {
+<<<<<<< HEAD
           res.json({"username":"username"});
         } else {
           res.json(null);
+=======
+          res.send('User exists.');
+        } else {
+          res.send(req.params.username);
+>>>>>>> add else to check username
         }
       })
       .fail(function (reason) {
