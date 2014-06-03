02c60121ef51c3e936252820e4db89b3a76a5a39
Amira Anuar <amiraanuar@gmail.com>
2014-06-02 19:10:59 -0700

    Create battle schema

--- server/battle/battle_model.js
+++ server/battle/battle_model.js
@@ -3,10 +3,9 @@
 var mongoose = require('mongoose');
 
 var BattleSchema = new mongoose.Schema({
-  _id       : String,
-  winner    : String,
-  players   : [],
-  createdAt : Date
+ winner    : String,
+ loser     : String,
+ createdAt : Date
 });
 
 module.exports = exports = mongoose.model('battles', BattleSchema);
