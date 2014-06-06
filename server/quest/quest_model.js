feb58d3d52be787f765648b254fc8b4e0227715b
Amira Anuar <amiraanuar@gmail.com>
2014-06-05 21:17:56 -0700

    Modify quest model to have short and long description

--- server/quest/quest_model.js
+++ server/quest/quest_model.js
@@ -3,17 +3,18 @@
 var mongoose = require('mongoose');
 
  var QuestSchema = new mongoose.Schema({
-    _id         :  String,
-    title       :  String,
-    description :  String,
-    numDays     :  Number,
-    numHours    :  Number,
-    gold        :  Number,
-    winGoals    :  Number,
-    experienceG :  Number,
-    difficulty  :  Number,
-    type        :  String,
-    activity    :  String
+    _id                :  String,
+    title              :  String,
+    shortDescription   :  String,
+    longDescription    :  String,
+    numDays            :  Number,
+    numHours           :  Number,
+    gold               :  Number,
+    winGoals           :  Number,
+    experienceG        :  Number,
+    difficulty         :  Number,
+    type               :  String,
+    activity           :  String
  });
 
  module.exports = exports = mongoose.model('quests', QuestSchema);
